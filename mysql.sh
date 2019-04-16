ddd8d69dbf8fce4cf707ca45b3110f987b602fdc
aftab70 <43289912+aftab70@users.noreply.github.com>
2019-04-16 15:21:40 +0530

    Update mysql.sh

--- mysql.sh
+++ mysql.sh
@@ -1,1 +1,4 @@
 mysql -h 10.10.10.12 -P 3306 -u haproxy_root -p123456 BOOKS -e 'INSERT INTO AUTHOR (name) VALUES("AFTAB");'
+
+
+mysqlslap --user=haproxy_root --password=123456 --host=10.10.10.12  --verbose --concurrency=100 --iterations=10 --create-schema=BOOKS --query="INSERT INTO AUTHOR (name) VALUES("AFTAB");"
